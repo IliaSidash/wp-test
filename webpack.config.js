ab6f364179f5261c11742b1396058b14e2f860b5
konstPialov <azure_dream@mail.ru>
2019-01-13 22:40:09 +0300

    fitst com

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+
+    entry: [
+        './src/js/index.js',
+        './src/sass/style.sass'
+    ],
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: './js/bundle.js'
+    },
+
+    devtool: 'source-map',
+    
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/sass'),
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            // sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer')({
+                                    // cascade: false
+                                })
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './css/style.bundle.css'
+        }),
+    ]
+};
